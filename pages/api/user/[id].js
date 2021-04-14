26e6b4813667c441c62d1dc7b8edfbd8ae8eed31
Alexander Jimenez <ajimenez@konradgroup.com>
2021-04-13 23:43:05 -0600

    user changes

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -1,27 +1,27 @@
-import { auth, database } from "../../firebase/app";
-import { COLLECTION_CLIENT, ERROR_WRONG_PASSWORD } from "../../../utils/consts";
+// import { auth, database } from "../../firebase/app";
+// import { COLLECTION_CLIENT, ERROR_WRONG_PASSWORD } from "../../../utils/consts";
 
-export default async (req, res) => {
-    const { method } = req;
-    switch (method) {
-        case 'GET':
-            try {
-                const { email } = req.query;
-                const userRef = database.collection(COLLECTION_CLIENT);
-                const userDB = await userRef.where('email', '==', email).get();
-                const user = {
-                    id: userDB.docs[0].id,
-                    email: userDB.docs[0].data().email,
-                    isAdmin: !!userDB.isAdmin
-                };
-                res.json(user);
-            } catch (error) {
-                if (error.code === ERROR_WRONG_PASSWORD) {
-                    res.status(400).json({ message: 'Contraseña errónea' })
-                } else {
-                    res.status(400).json({ message: 'Ocurrio un error al registrar' })
-                }
-            }
-            break;
-    }
-}
+// export default async (req, res) => {
+//     const { method } = req;
+//     switch (method) {
+//         case 'GET':
+//             try {
+//                 const { email } = req.query;
+//                 const userRef = database.collection(COLLECTION_CLIENT);
+//                 const userDB = await userRef.where('email', '==', email).get();
+//                 const user = {
+//                     id: userDB.docs[0].id,
+//                     email: userDB.docs[0].data().email,
+//                     isAdmin: !!userDB.isAdmin
+//                 };
+//                 res.json(user);
+//             } catch (error) {
+//                 if (error.code === ERROR_WRONG_PASSWORD) {
+//                     res.status(400).json({ message: 'Contraseña errónea' })
+//                 } else {
+//                     res.status(400).json({ message: 'Ocurrio un error al registrar' })
+//                 }
+//             }
+//             break;
+//     }
+// }
